23e119444c333917738b47f66e36c353b457ef68
Thoboyy <thobou@gmail.com>
2022-10-18 23:03:39 +0200

    commit tp5

--- src/main/java/fr/webapp/j3/SumServlet.java
+++ src/main/java/fr/webapp/j3/SumServlet.java
@@ -1,20 +1,21 @@
 package fr.webapp.j3;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.apache.avalon.framework.configuration.Configuration;
-import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.avalon.framework.configuration.DefaultConfiguration;
-import org.krysalis.barcode4j.BarcodeException;
-import org.krysalis.barcode4j.BarcodeGenerator;
-import org.krysalis.barcode4j.BarcodeUtil;
+import org.krysalis.barcode4j.impl.code39.Code39Bean;
 import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
-
+import org.krysalis.barcode4j.tools.UnitConv;
 /**
  * Servlet implementation class SumServlet
  */
@@ -45,7 +46,30 @@ public class SumServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		//Create the barcode bean
+		Code39Bean bean = new Code39Bean();
+
+		final int dpi = 150;
+
+		//Configure the barcode generator
+		bean.setModuleWidth(UnitConv.in2mm(1.0f / dpi)); //makes the narrow bar 
+		                                                 //width exactly one pixel
+		bean.setWideFactor(3);
+		bean.doQuietZone(false);
+
+		//Open output file
+		File outputFile = new File("qrcode.png");
+		OutputStream out = new FileOutputStream(outputFile);
+		try {
+		    BitmapCanvasProvider canvas = new BitmapCanvasProvider(
+		            out, "image/x-png", dpi, BufferedImage.TYPE_BYTE_BINARY, false, 0);
+	        String chaine = request.getParameter("nomproduit")+request.getParameter("idproduit");
+		    bean.generateBarcode(canvas, chaine);
+		    canvas.finish();
+		} finally {
+		    out.close();
+		}
 	}
 
 }
